0a571a0d7e641a4ef8437874522cdfd4e0de8c1c
Watcharapol <59160609@go.buu.ac.th>
2022-01-24 22:11:07 +0700

    'add crd not u'

--- react-crud/server/index.js
+++ react-crud/server/index.js
@@ -24,6 +24,25 @@ app.get('/customer',(req,res) => {
     });
 });
 
+app.post('/create',(req,res) => {
+    var sql = `INSERT INTO customer (first_name,last_name, age) VALUES ('${req.body.first_name}', '${req.body.last_name}',${req.body.age})`;
+    db.query(sql, function (err, result) {
+    if (err) throw err;
+        console.log("1 record inserted, ID: " + result.insertId);
+    });
+});
+
+app.delete('/customer/:id',(req,res) => {
+    var sql = `DELETE FROM customer WHERE id = '${req.params.id}'`
+    db.query(sql, function (err, result) {
+        if (err) throw err;
+            console.log("1 record inserted, ID: " + result.insertId);
+        res.send(result)
+        });
+        
+    
+})
+
 app.listen('3001',() => {
     console.log('Server is running on port 3001');
 })
